778e1b2efdaf2259dc37de06a7fbf4074e77fe03
Oniely <jabagatniel09@gmail.com>
2025-03-14 13:21:19 +0800

    not-found page, tailwind configure font & color

---
+++ serverless_frontend/src/components/NotFound.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+	return (
+		<div className="content-center w-full h-dvh">
+			<div className="max-w-xl mx-auto text-center">
+				<h1 className="font-mono text-4xl">404 Page Not Found.</h1>
+				<p className="font-sans text-sm">
+					Sorry, the page you are looking for could not be found.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default NotFound;
--- serverless_frontend/tailwind.config.js
+++ serverless_frontend/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter var", "sans-serif"],
+				mono: ["Lexend", "monospace"],
+			},
+			colors: {
+				primary: "#eab308",
+			},
+		},
+	},
+	plugins: [],
+};
